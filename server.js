d5a1fd40e35e97773d1234fad199d843cf292643
wariormex <wariormex@hotmail.com>
2022-04-04 16:17:18 -0500

    Added React Front End v0.1

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
+const cors = require('cors')
 const express = require ('express');
 const mongoose = require('mongoose');
 
 //Creacion del server
 const app = express();
-const port = 3000;
+const port = 4000;
 
 //Coneccion a la db
 mongoose.connect(process.env.DATABASE_URL_CLOUD, {useNewUrlParser: true});
@@ -15,6 +16,7 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log("Conectado a la base de datos"));
 
 //Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
